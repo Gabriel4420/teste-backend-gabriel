e427d1b0340c25f59a7f7fe91a6cbbbc7b84d89b
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2024-01-07 23:57:59 -0300

    feat: :sparkles: update service  adding try catch validation

--- src/services/resultado.service.ts
+++ src/services/resultado.service.ts
@@ -5,13 +5,21 @@ import { IResultadoService } from "../interfaces/iResultadoService";
 
 let result;
 export class ResultadoService implements IResultadoService {
-  async listarResultado(id: string): Promise<any> {
-    result = await database.resultado.findUnique({ where: { id } });
-    return result;
+  async listarResultado(id: string): Promise<Resultado | any> {
+    try {
+      result = await database.resultado.findUnique({ where: { id } });
+      return result;
+    } catch (error) {
+      console.log("Erro ao buscar resultado: ", error);
+    }
   }
-  async listarResultados(): Promise<any> {
-    result = await database.resultado.findMany();
-    return result;
+  async listarResultados(): Promise<Resultado | any> {
+    try {
+      result = await database.resultado.findMany();
+      return result;
+    } catch (error) {
+      console.log("Erro ao buscar resultados: ", error);
+    }
   }
   async registrarResultado({
     nota,
@@ -19,19 +27,31 @@ export class ResultadoService implements IResultadoService {
     disciplina,
     criadoEm,
   }: IResultadoProps): Promise<Resultado | any> {
-    result = await database.resultado.create({
-      data: {
-        nota,
-        bimestre: bimestre as unknown as Bimestre,
-        disciplina: disciplina as unknown as Disciplina,
-        criadoEm: (criadoEm = new Date() as unknown as Date),
-      },
-    });
+    try {
+      if (nota < 0 || nota > 10) {
+        throw new Error("Nota deve ser entre 0 e 10");
+      }
 
-    return result;
+      result = await database.resultado.create({
+        data: {
+          nota,
+          bimestre: bimestre as unknown as Bimestre,
+          disciplina: disciplina as unknown as Disciplina,
+          criadoEm: (criadoEm = new Date() as unknown as Date),
+        },
+      });
+
+      return result;
+    } catch (error) {
+      console.log("Erro ao registrar resultado: ", error);
+    }
   }
-  async removerResultado(id: string): Promise<any> {
-    result = await database.resultado.delete({ where: { id } });
-    return result;
+  async removerResultado(id: string): Promise<Resultado | any> {
+    try {
+      result = await database.resultado.delete({ where: { id } });
+      return result;
+    } catch (error) {
+      console.log("Erro ao remover resultado: ", error);
+    }
   }
 }
